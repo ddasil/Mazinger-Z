a862b900d1547065a645e86af9383d1422cc8795
ddasil <ddasill0330@gmail.com>
2025-05-08 13:15:21 +0900

    dayoung 브랜치에서 변경된 파일을 main 브랜치에 반영

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from . import views
+from .views import preference_view
+
 
 urlpatterns = [
     path('', views.main, name='main'),
@@ -10,4 +12,6 @@ urlpatterns = [
     path('analyze/', include('analyze.urls')),  # analyze 앱 연결
     path('music/', include('music_search.urls')), # music_search 앱 연결
     path('recommend/', include('recommendations.urls')), # recommendations 앱연결
+    path('preference/', preference_view, name='preference'), # 메인 음악 취향 검사
+    path("recommend_by_genre/", views.recommend_by_genre, name="recommend_by_genre"), # 메인 음악 취향 검사 추천 음악
 ]
