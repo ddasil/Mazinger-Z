80cabc853b98784cdfca36a2f2571656c9b1535d
ddasil <ddasill0330@gmail.com>
2025-05-08 12:32:11 +0900

    진섭&동건 페이지 병합

--- music_search/admin.py
+++ music_search/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import TaggedSong, FavoriteSong
 
-# Register your models here.
+@admin.register(TaggedSong)
+class TaggedSongAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist', 'tags')
+    search_fields = ('title', 'artist')
+
+@admin.register(FavoriteSong)
+class FavoriteSongAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist', 'user', 'created_at')
+    search_fields = ('title', 'artist', 'user__username')
+
+    list_filter = ['user'] 
--- music_search/models.py
+++ music_search/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class TaggedSong(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    lyrics = models.TextField()
+    tags = models.JSONField(default=list)  # ["운동", "영화", "산책"]
+
+    def __str__(self):
+        return f"{self.title} - {self.artist}"
+    
+class FavoriteSong(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album_cover_url = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'title', 'artist')  # 중복 방지
+
+    def __str__(self):
+        return f"{self.user.username} - {self.title} by {self.artist}"
